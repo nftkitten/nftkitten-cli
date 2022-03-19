78cf282eeaa0a37e4bb1c9d1fdcef5accaa4b185
nftkitten <nftkitten+github@pm.me>
2022-03-19 20:10:54 +0800

    better log

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalln("Error loading .env file")
 	}
 
 	var rootCmd = &cobra.Command{Use: "app"}
