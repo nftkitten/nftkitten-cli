43d83ccde99b431ba04acb48afd334febe24fa8d
nftkitten <nftkitten+github@pm.me>
2022-03-20 19:51:34 +0800

    stop loading activity

--- cmd/me/collection.go
+++ cmd/me/collection.go
@@ -27,9 +27,9 @@ func subscribeCollection(
 		pool = append(pool, fetchMany(url, fmt.Sprint("collections/", symbol, "/listings"), 20).
 			// ForEach(subscribeCollectionListing(symbol, tokenMintsPub, walletAddressesPub), logError, doNothing, rxgo.WithCPUPool()))
 			ForEach(subscribeCollectionListing(symbol, filter), logError, doNothing, rxgo.WithCPUPool()))
-		pool = append(pool, fetchMany(url, fmt.Sprint("collections/", symbol, "/activities"), 500).
-			// ForEach(subscribeCollectionActivity(symbol, tokenMintsPub, walletAddressesPub, rxgo.WithCPUPool()), logError, doNothing, rxgo.WithCPUPool()))
-			ForEach(subscribeCollectionActivity(symbol, filter), logError, doNothing, rxgo.WithCPUPool()))
+		// pool = append(pool, fetchMany(url, fmt.Sprint("collections/", symbol, "/activities"), 500).
+		// 	// ForEach(subscribeCollectionActivity(symbol, tokenMintsPub, walletAddressesPub, rxgo.WithCPUPool()), logError, doNothing, rxgo.WithCPUPool()))
+		// 	ForEach(subscribeCollectionActivity(symbol, filter), logError, doNothing, rxgo.WithCPUPool()))
 		pool = append(pool, fetchOne(url, fmt.Sprint("collections/", symbol, "/stats")).
 			ForEach(subscribeCollectionStat(symbol, filter), logError, doNothing, rxgo.WithCPUPool()))
 	}
@@ -65,40 +65,40 @@ func subscribeCollectionListing(
 	}
 }
 
-func subscribeCollectionActivity(
-	symbol string,
-	// tokenMintsPub chan rxgo.Item,
-	// walletAddressesPub chan rxgo.Item,
-	filter func(string) bool,
-) func(interface{}) {
-	return func(item interface{}) {
-		m := item.(map[string]interface{})
-		bytes, _ := json.Marshal(item)
-		signature := fmt.Sprint(m["signature"])
-		scanId := fmt.Sprint("collection_activity.", symbol, ".", signature)
-		if filter(scanId) {
-			pool = append(pool, dbExecuteMany(
-				sqlForUpsertWithParent(
-					"collection",
-					"activity",
-					symbol,
-					bytes,
-					signature,
-				),
-				sqlForUpsertScanLog(scanId),
-			).ForEach(doNothingOnNext, logError, doNothing, rxgo.WithCPUPool()))
-		}
-		// if fmt.Sprint(m["tokenMint"]) != "" {
-		// 	tokenMintsPub <- rxgo.Item{V: fmt.Sprint(m["tokenMint"])}
-		// }
-		// if fmt.Sprint(m["buyer"]) != "" {
-		// 	walletAddressesPub <- rxgo.Item{V: fmt.Sprint(m["buyer"])}
-		// }
-		// if fmt.Sprint(m["seller"]) != "" {
-		// 	walletAddressesPub <- rxgo.Item{V: fmt.Sprint(m["seller"])}
-		// }
-	}
-}
+// func subscribeCollectionActivity(
+// 	symbol string,
+// 	// tokenMintsPub chan rxgo.Item,
+// 	// walletAddressesPub chan rxgo.Item,
+// 	filter func(string) bool,
+// ) func(interface{}) {
+// 	return func(item interface{}) {
+// 		m := item.(map[string]interface{})
+// 		bytes, _ := json.Marshal(item)
+// 		signature := fmt.Sprint(m["signature"])
+// 		scanId := fmt.Sprint("collection_activity.", symbol, ".", signature)
+// 		if filter(scanId) {
+// 			pool = append(pool, dbExecuteMany(
+// 				sqlForUpsertWithParent(
+// 					"collection",
+// 					"activity",
+// 					symbol,
+// 					bytes,
+// 					signature,
+// 				),
+// 				sqlForUpsertScanLog(scanId),
+// 			).ForEach(doNothingOnNext, logError, doNothing, rxgo.WithCPUPool()))
+// 		}
+// 		// if fmt.Sprint(m["tokenMint"]) != "" {
+// 		// 	tokenMintsPub <- rxgo.Item{V: fmt.Sprint(m["tokenMint"])}
+// 		// }
+// 		// if fmt.Sprint(m["buyer"]) != "" {
+// 		// 	walletAddressesPub <- rxgo.Item{V: fmt.Sprint(m["buyer"])}
+// 		// }
+// 		// if fmt.Sprint(m["seller"]) != "" {
+// 		// 	walletAddressesPub <- rxgo.Item{V: fmt.Sprint(m["seller"])}
+// 		// }
+// 	}
+// }
 
 func subscribeCollectionStat(
 	symbol string,
